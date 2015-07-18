dc8e0b4ca6818eab97aa6028ccd2cdef96f102e0
Nabil Freeman <nabil@matchchat.co.uk>
2015-07-18 23:32:39 +0200

    stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+NodeList.prototype.forEach = Array.prototype.forEach;
+
 var _FW = {};
 
 //muscle code
@@ -19,6 +21,8 @@ var changeMuscle = function(){
 	// emojiNumber++;
 };
 
+changeMuscle();
+
 var shuffle = function(array){
 	var currentIndex = array.length, temporaryValue, randomIndex ;
 
@@ -38,180 +42,28 @@ var shuffle = function(array){
 	return array;
 }
 
-// //load workouts
-// var httpCallback = function(){
-// 	_FW.workouts = JSON.parse(this.responseText)["workouts"];
-// 	_FW.workouts = shuffle(_FW.workouts);
-//
-// 	document.querySelector("header .wrap").addEventListener("click", function(){
-// 		render("home", _FW.workouts, true);
-// 	});
-//
-// 	var path = location.pathname;
-// 	if(path === "/"){
-// 		render("home", _FW.workouts);
-//
-// 		var url = "/";
-//
-// 		history.replaceState({
-// 			"pageType":"home",
-// 			"pageData":_FW.workouts
-// 		}, null, url);
-// 	} else {
-// 		//strip query strings, remove /, replace - with a space
-// 		path = path.split("?")[0].split("/").join("").split("-").join(" ");
-//
-// 		_FW.workouts.forEach(function(workout){
-// 			if(workout.name.toLowerCase() === path){
-// 				render("workout", workout);
-//
-// 				//format url
-// 				var url = workout.name.toLowerCase().split(" ").join("-");
-//
-// 				history.replaceState({
-// 					"pageType":"workout",
-// 					"pageData":workout
-// 				}, null, url);
-// 			}
-// 		});
-// 	}
-//
-// 	window.addEventListener("popstate", function(e) {
-// 		if(e.state === null){
-// 			render("home", _FW.workouts);
-// 		} else {
-// 	    	render(e.state.pageType, e.state.pageData);
-// 		}
-// 	});
-//
-// };
-//
-// var httpRequest = new XMLHttpRequest();
-// httpRequest.onload = httpCallback;
-// httpRequest.open("get", "/workouts.json", true);
-// httpRequest.send();
+var celebs = document.querySelector(".workout_thumbs");
 
-var render = function(pageType, pageData, triggerState){
-	var page = document.createElement("div");
-	page.id = "page";
+console.log("hey");
 
-	switch(pageType){
-		case "home":
+if(celebs !== null){
+	var tiles = celebs.querySelector(".workout_tile");
 
-			// var menu = document.createElement("div");
-			// menu.id = "menu";
-			// menu.innerHTML = "<button>Guys</button><button disabled>Girls</button><button>Random</button>";
-			// page.appendChild(menu);
+	console.log(tiles);
 
-			pageData.forEach(function(workout){
-				var el = document.createElement("div");
-				el.className = "workout_tile";
+	shuffled = shuffle(tiles);
 
-				if(workout.photo === undefined){
-					workout.photo = "/images/workouts/" + workout.name.toLowerCase().replace(/ /g, "-") + ".jpg";
-				}
+	console.log(shuffled);
 
-				el.innerHTML = '<img src="' + workout.photo + '"><div class="name">' + workout.name + '</div>';
-				el.__workout = workout;
+	var div = document.createElement("div");
+	shuffled.forEach(function(tile){
+		div.appendChild(tile);
+	});
 
-				el.addEventListener("click", function(e){
-					render("workout", el.__workout, true);
-				});
+	console.log(div);
 
-				page.appendChild(el);
-			});
-
-			//format url
-			var url = "/";
-			var title = "Famous Workouts!";
-			document.querySelector("title").innerHTML = title;
-
-			if(window.ga !== undefined){
-				window.ga('send', 'pageview', {
-					'page': url,
-					'title': title
-				});
-			}
-
-			if(triggerState){
-				history.pushState({
-					"pageType":pageType,
-					"pageData":pageData
-				}, null, url);
-			}
-
-		break;
-		case "workout":
-
-			if(pageData.photo === undefined){
-				pageData.photo = "/images/workouts/" + pageData.name.toLowerCase().replace(/ /g, "-") + ".jpg";
-			}
-
-			var workout_page_frame = document.createElement("div");
-			workout_page_frame.className = "workout_page_frame";
-			workout_page_frame.innerHTML =
-				'<div class="frame_border">'
-				+ '<div class="frame_inset">'
-					+ '<img src="' + pageData.photo + '">'
-					+ '<div class="name">' + pageData.name + '</div>'
-				+ '</div>'
-			+ '</div>';
-
-			var table = document.createElement("table");
-
-			var row = document.createElement("tr");
-			row.className = "heading";
-			row.innerHTML = '<td>Exercise</td><td>Sets x Reps</td';
-			table.appendChild(row);
-
-			pageData.routines.forEach(function(routine){
-				var row = document.createElement("tr");
-				row.innerHTML = '<td>' + routine.name + '</td><td>' + routine.amount + '</td';
-				table.appendChild(row);
-			});
-
-			workout_page_frame.appendChild(table);
-
-			page.appendChild(workout_page_frame);
-
-			//format url
-			var url = pageData.name.toLowerCase().split(" ").join("-");
-			var title = pageData.name + "'s Epic Workout - Famous Workouts!";
-			document.querySelector("title").innerHTML = title;
-
-			if(window.ga !== undefined){
-				window.ga('send', 'pageview', {
-					'page': url,
-					'title': title
-				});
-			}
-
-			if(triggerState){
-				history.pushState({
-					"pageType":pageType,
-					"pageData":pageData
-				}, null, url);
-			}
-
-		break;
-		default:
-
-		break;
-	}
-
-	page.className = "hidden";
-
-	var oldPage = document.getElementById("page");
-	oldPage.parentNode.replaceChild(page, oldPage);
-
-	window.document.body.scrollTop = 0;
-
-	changeMuscle();
-
-	setTimeout(function(){
-		page.className = "";
-	}, 250);
-};
+	document.querySelector(".workout_thumbs").innerHTML = div.innerHTML;
+}
 
 //extend array
 Array.prototype.contains = function(obj) {
